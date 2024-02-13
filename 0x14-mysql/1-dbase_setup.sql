f1579c746ed5f55a7e7a7faea0d293f025340f51
codeegirl0 <asmaelk65@gmail.com>
2024-02-13 09:59:03 +0100

    files

---
+++ 0x14-mysql/1-dbase_setup.sql
@@ -0,0 +1,4 @@
+-- Creating a user with a rep
+CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
---
+++ 0x14-mysql/README.md
@@ -0,0 +1,1 @@
+alx mysql roject
