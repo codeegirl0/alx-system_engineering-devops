5e4bb1aabf563d3cddf568d204c3cb40f2ae3a1a
codeegirl0 <asmaelk65@gmail.com>
2024-03-11 22:44:37 +0000

    files

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,14 @@
+# enabling the user holberton to log and open
+
+# add file limit for user 
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+
+# add soft file limit 
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
